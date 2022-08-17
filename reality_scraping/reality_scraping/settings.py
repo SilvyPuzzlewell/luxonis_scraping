c70f6f745f8d7cce7c538c472e393dee7d22ae91
sylvi <kristron6@gmail.com>
2022-08-17 20:33:01 +0200

    pipeline enabled

--- reality_scraping/reality_scraping/settings.py
+++ reality_scraping/reality_scraping/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    #'pipelines.RealityScrapingPipeline': 300,
+    'pipelines.RealityScrapingPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
